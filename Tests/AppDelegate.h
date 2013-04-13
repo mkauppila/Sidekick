8a22dc0add2b8d58ae6466dde7dabb685e6edca3
Markus Kauppila <markus.kauppila@gmail.com>
2013-04-13 14:09:59 +0300

    Restructure the project and add Xcode project for unit testing

---
+++ Tests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sidekick
+//
+//  Created by markus on 13.4.2013.
+//  Copyright (c) 2013 Markus Kauppila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sidekick
+//
+//  Created by markus on 13.4.2013.
+//  Copyright (c) 2013 Markus Kauppila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
