a6978ddc148fb3291f40128b06f6e3449b1f8725
Markus Kauppila <markus.kauppila@gmail.com>
2013-08-31 13:26:31 +0300

    Add test for filteredArrayUsingBlock

--- Tests/ArrayTest.m
+++ Tests/ArrayTest.m
@@ -65,4 +65,20 @@
 	}
 }
 
+- (void)testFilteredArrayUsingBlock
+{
+	NSArray *array = @[@1, @2, @3, @4, @5, @6, @7, @8, @9, @10];
+	NSArray *correctResult = @[@2, @4, @6, @8, @10];
+	
+	NSArray *result = [array filteredArrayUsingBlock:^BOOL(id value) {
+		return [value integerValue] % 2 == 0;
+	}];
+	
+	for (NSUInteger counter = 0; counter < result.count; ++counter) {
+		GHAssertEqualObjects([correctResult objectAtIndex:counter],
+							 [result objectAtIndex:counter],
+							 @"Elements should match");
+	}
+}
+
 @end
